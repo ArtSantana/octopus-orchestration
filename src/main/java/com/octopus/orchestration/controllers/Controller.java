6b3967b32c525fddcc36923f977d8572bda771dd
Vitor Muniz <vitor.muniz@sensedia.com>
2022-03-04 11:31:46 -0300

    adding hello world

--- src/main/java/com/octopus/orchestration/controllers/Controller.java
+++ src/main/java/com/octopus/orchestration/controllers/Controller.java
@@ -1,8 +1,13 @@
 package com.octopus.orchestration.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
 }

44fabc11b4491ef827229277f8b461927c9dd539
Vitor Muniz <vitor.muniz@sensedia.com>
2022-03-04 18:01:09 -0300

    implementing container controller crud

--- src/main/java/com/octopus/orchestration/controllers/ContainersController.java
+++ src/main/java/com/octopus/orchestration/controllers/ContainersController.java
@@ -2,20 +2,42 @@ package com.octopus.orchestration.controllers;
 
 import com.octopus.orchestration.services.ContainersService;
 import com.spotify.docker.client.messages.Container;
+import com.spotify.docker.client.messages.ContainerInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/containers")
 public class ContainersController {
 
     @Autowired
     private ContainersService containersService;
 
     @GetMapping
-    public List<Container> listAllContainers()  {
-        return containersService.listAll();
+    public ResponseEntity<List<Container>> listAll()  {
+        return ResponseEntity.ok(containersService.listAll());
+    }
+
+    @GetMapping("/inspect/{id}")
+    public ResponseEntity<ContainerInfo> inspect(@PathVariable String id) {
+        return ResponseEntity.ok(containersService.inspect(id));
+    }
+
+    @GetMapping("/logs/{id}")
+    public ResponseEntity<String> getLogs(@PathVariable String id) {
+        return ResponseEntity.ok(containersService.getLogs(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        containersService.delete(id);
+        ResponseEntity.noContent();
     }
 }
--- src/main/java/com/octopus/orchestration/services/ContainersService.java
+++ src/main/java/com/octopus/orchestration/services/ContainersService.java
@@ -4,8 +4,11 @@ import com.octopus.orchestration.dockerclient.DockerClient;
 import com.octopus.orchestration.exceptions.ContainersException;
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerClient.ListContainersParam;
+import com.spotify.docker.client.DockerClient.LogsParam;
+import com.spotify.docker.client.LogStream;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.Container;
+import com.spotify.docker.client.messages.ContainerInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -23,4 +26,29 @@ public class ContainersService {
             throw new ContainersException(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
+    public ContainerInfo inspect(String id) {
+        try {
+            return dockerClient.inspectContainer(id);
+        } catch (DockerException | InterruptedException e) {
+            throw new ContainersException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public void delete(String id) {
+        try {
+            dockerClient.removeContainer(id);
+        } catch (DockerException | InterruptedException e) {
+            throw new ContainersException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public String getLogs(String id) {
+        try (LogStream stream = dockerClient.logs(id, LogsParam.stdout(), LogsParam.stderr())) {
+            return stream.readFully();
+        } catch (DockerException | InterruptedException e) {
+            throw new ContainersException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }

ad765e3612fd62d62ffd1605f2ee3e5ed048f25c
Arthur Santana <arthur.santana@sensedia.com>
2022-03-06 17:27:23 -0300

    Added log4j to the application

--- src/main/java/com/octopus/orchestration/controllers/ContainersController.java
+++ src/main/java/com/octopus/orchestration/controllers/ContainersController.java
@@ -2,20 +2,20 @@ package com.octopus.orchestration.controllers;
 
 import com.octopus.orchestration.services.ContainersService;
 import com.spotify.docker.client.messages.Container;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-public class ContainersController {
+@RestController public class ContainersController {
+	Logger logger = Logger.getLogger(ContainersController.class);
 
-    @Autowired
-    private ContainersService containersService;
+	@Autowired private ContainersService containersService;
 
-    @GetMapping
-    public List<Container> listAllContainers()  {
-        return containersService.listAll();
-    }
+	@GetMapping public List<Container> listAllContainers() {
+		logger.info("[GET] - listAllContainers");
+		return containersService.listAll();
+	}
 }

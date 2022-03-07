9f3fa6046282aa55479e71967b4ede00c0a9f0a7
Vitor Muniz <vitormuuniz@gmail.com>
2022-03-06 22:56:55 -0300

    adding logger

--- src/main/java/com/octopus/orchestration/exceptions/ContainersException.java
+++ src/main/java/com/octopus/orchestration/exceptions/ContainersException.java
@@ -2,6 +2,7 @@ package com.octopus.orchestration.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.io.Serial;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ContainersException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private String message;
--- src/main/java/com/octopus/orchestration/exceptions/ContainersExceptionHandler.java
+++ src/main/java/com/octopus/orchestration/exceptions/ContainersExceptionHandler.java
@@ -4,11 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.apache.log4j.Logger;
+
 @RestControllerAdvice
 public class ContainersExceptionHandler {
 
+    private static final Logger LOGGER = Logger.getLogger(ContainersExceptionHandler.class);
+
     @ExceptionHandler({ContainersException.class})
     protected ResponseEntity<Object> handleBaseException(ContainersException ex) {
+        LOGGER.error(ex.getMessage());
         return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
     }
 }
